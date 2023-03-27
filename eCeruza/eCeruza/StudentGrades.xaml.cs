e21b6dc30d46c7123b54c538bd91616cd0ae4d74
somloidavid <somloi.david@students.jedlik.eu>
2023-03-27 18:29:47 +0200

    Scrollable Grades Panel

--- eCeruza/eCeruza/StudentGrades.xaml.cs
+++ eCeruza/eCeruza/StudentGrades.xaml.cs
@@ -17,14 +17,23 @@ namespace eCeruza
 {
     public partial class StudentGrades : Window
     {
+        public List<Grade> gradesInSubject = new();
         public StudentGrades()
         {
+            foreach (Grade grade in MainWindow.User.Grades)
+            {
+                if (grade.Subject == StudentClasses.SelectedSubject)
+                {
+                    gradesInSubject.Add(grade);
+                }
+            }
             InitializeComponent();
             FillContent();
         }
 
         public void FillContent()
         {
+            grd_Grades.Height = gradesInSubject.Count * 150;
             Teacher teacher = new Teacher();
             lbl_Subject.Content = StudentClasses.SelectedSubject;
             foreach (Teacher tchr in MainWindow.Teachers)
@@ -38,13 +47,38 @@ namespace eCeruza
                 }
             }
             lbl_Teacher.Content = teacher.Name;
-            foreach (RowDefinition rd in grd_Grades.RowDefinitions)
+            for (int i = 0; i < gradesInSubject.Count; i++)
             {
-                rd.Height = new GridLength(grd_Grades.Height / 6);
+                RowDefinition rd = new();
+                rd.Height = new GridLength(150);
+                grd_Grades.RowDefinitions.Add(rd);
+                Grid griddy = new();
+                griddy.Width = 800;
+                griddy.Height = 900 / 6;
+                griddy.Width = 800;
+                grd_Grades.Children.Add(griddy);
+                Grid.SetRow(griddy, i);
+                Label lbl_Value = new();
+                Label lbl_Message = new();
+                Label lbl_Date = new();
+                lbl_Value.Content = gradesInSubject[i].Value;
+                lbl_Message.Content = gradesInSubject[i].Message;
+                lbl_Date.Content = gradesInSubject[i].Date;
+                lbl_Value.Width = 50;
+                lbl_Message.Width = 300;
+                lbl_Date.Width = 400;
+                lbl_Value.Height = 900 / 6;
+                lbl_Message.Height = 900 / 6;
+                lbl_Date.Height = 900 / 6;
+                lbl_Value.Margin = new Thickness(0, 0, 750, 0);
+                lbl_Date.Margin = new Thickness(600, 0, 0, 0);
+                lbl_Value.FontSize = 34;
+                lbl_Message.FontSize = 34;
+                lbl_Date.FontSize = 34;
+                griddy.Children.Add(lbl_Value);
+                griddy.Children.Add(lbl_Message);
+                griddy.Children.Add(lbl_Date);
             }
-            Label lb = new();
-            lb.Content = "dhoaiwd";
-            Grid.SetRow(lb, 6);
         }
     }
 }
