0c20f3baec2445290766decd6acbf136752b9ab0
Sándor Kevin <sandor.kevin@students.jedlik.eu>
2023-03-31 11:42:58 +0200

    login redesign naon cool

--- eCeruza/eCeruza/MainWindow.xaml.cs
+++ eCeruza/eCeruza/MainWindow.xaml.cs
@@ -102,5 +102,22 @@ namespace eCeruza
                 MessageBox.Show("Helytelen Jelszó!");
             }
         }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
     }
 }
