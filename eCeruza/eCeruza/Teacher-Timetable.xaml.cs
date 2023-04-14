b106b48b2f4877300ed92ce905cf625515105b62
Viktor <slavik00757@gmail.com>
2023-04-14 12:28:01 +0200

    otthon következik a folytatás, kell az órarend és kell idegennyelv és vizualitas

--- eCeruza/eCeruza/Teacher-Timetable.xaml.cs
+++ eCeruza/eCeruza/Teacher-Timetable.xaml.cs
@@ -24,11 +24,28 @@ namespace eCeruza
     {
         //List<Dictionary<string, Dictionary<string, string>>> z = JsonSerializer.Deserialize<>(File.ReadAllText("Source/TimeTable.json")).ToList();
         List<Dictionary<string, Dictionary<int, string>>> s =new List<Dictionary<string, Dictionary<int, string>>> { new Dictionary<string, Dictionary<int, string>> { { "Hétfő", new Dictionary<int, string> { { 1, "Idegennyelv" }, { 2, "Idegennyelv" }, { 3, "Matematika"}, { 4, "Fizika"}, { 5, "Programozás"}, { 6, "Programozás"} } } } };
+        string[,] Orarend = new string[,]{
+            {"Idegennyelv","Idegennyelv","Matematika","Fizika","Programozás","Programozás" },
+            {"IKT Projektmunka","IKT Projektmunka","Történelem","Irodalom","Nyelvtan","Idegennyelv"},
+            {"Programozás","Programozás","Irodalom","Történelem","Idegennyelv","Nyelvtan" },
+            {"Matematika","Matematika","Irodalom","Történelem","Programozás","Programozás" },
+            {"Idegennyelv","Idegennyelv","Programozás","Programozás","IKT Projektmunka","IKT Projektmunka"}};
         public Teacher_Timetable()
         {
             InitializeComponent();
             DateTime now = DateTime.Now;
             MessageBox.Show($"{now.DayOfWeek}");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    Label l = new Label();
+                    l.Content = Orarend[i, j];
+                    Grid.SetRow(l, j);
+                    Grid.SetColumn(l, i);
+                    TimeTable.Children.Add(l);
+                }
+            }
         }
     }
 }
