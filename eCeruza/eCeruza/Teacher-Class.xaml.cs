e681a549ecf66069136c15c3eb28972e5e67b4c0
Sándor Kevin <sandor.kevin@students.jedlik.eu>
2023-04-16 12:11:11 +0200

    teaches class grid base

---
+++ eCeruza/eCeruza/Teacher-Class.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace eCeruza
+{
+    /// <summary>
+    /// Interaction logic for Teacher_Class.xaml
+    /// </summary>
+    public partial class Teacher_Class : Window
+    {
+        public Teacher_Class()
+        {
+            InitializeComponent();
+        }
+    }
+}
